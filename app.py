6019e3f3ec0cc89dd4292d30cda98ec7c1badeac
HarshalLoya <155369134+HarshalLoya@users.noreply.github.com>
2024-09-08 18:05:12 +0000

    added dynamic pages to each job

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_with_id
 
 app = Flask(__name__)
 
@@ -14,6 +14,15 @@ def hello_world():
 def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
+
+
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_with_id(id)
+  if not job:
+    return "Not Found", 404
+  else:
+    return render_template('jobpage.html', job=job)
 
 
 @app.route('/faqs')
--- database.py
+++ database.py
@@ -1,26 +1,55 @@
 import pymysql
 import os
 
-def load_jobs_from_db():
-  timeout = 10
-  connection = pymysql.connect(
-      charset="utf8mb4",
-      connect_timeout=timeout,
-      cursorclass=pymysql.cursors.DictCursor,
-      db=os.environ['DB_CONN_NAME'],
-      host=os.environ['DB_CONN_HOSTNAME'],
-      password=os.environ['DB_CONN_PASS'],
-      read_timeout=timeout,
-      port=11166,
-      user=os.environ['DB_CONN_USER'],
-      write_timeout=timeout,
-  )
+timeout = 10
 
+
+def load_jobs_from_db():
   try:
+    connection = pymysql.connect(
+        charset="utf8mb4",
+        connect_timeout=timeout,
+        cursorclass=pymysql.cursors.DictCursor,
+        db=os.environ['DB_CONN_NAME'],
+        host=os.environ['DB_CONN_HOSTNAME'],
+        password=os.environ['DB_CONN_PASS'],
+        read_timeout=timeout,
+        port=11166,
+        user=os.environ['DB_CONN_USER'],
+        write_timeout=timeout,
+    )
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM jobs")
     result = cursor.fetchall()
-  finally:
     connection.close()
-
+  finally:
+    pass
+    
   return result
+
+
+def load_job_with_id(id):
+  try:
+    connection = pymysql.connect(
+        charset="utf8mb4",
+        connect_timeout=timeout,
+        cursorclass=pymysql.cursors.DictCursor,
+        db=os.environ['DB_CONN_NAME'],
+        host=os.environ['DB_CONN_HOSTNAME'],
+        password=os.environ['DB_CONN_PASS'],
+        read_timeout=timeout,
+        port=11166,
+        user=os.environ['DB_CONN_USER'],
+        write_timeout=timeout,
+    )
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT * FROM jobs WHERE id = {id}")
+    result = cursor.fetchall()
+    connection.close()
+  finally:
+    pass
+    
+  if len(result) == 0:
+    return None
+  else:
+    return result[0]
