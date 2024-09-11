a59ba0e40a5b022a09a2e7a390252436c96b61f5
HarshalLoya <155369134+HarshalLoya@users.noreply.github.com>
2024-09-11 10:29:24 +0000

    added application form and stored them in the database

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db, load_job_with_id
+from flask import Flask, render_template, jsonify, request
+from database import load_jobs_from_db, load_job_with_id, add_application_to_db
 
 app = Flask(__name__)
 
@@ -23,6 +23,14 @@ def show_job(id):
     return "Not Found", 404
   else:
     return render_template('jobpage.html', job=job)
+
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  data = request.form
+  job = load_job_with_id(id)
+  add_application_to_db(id, data)
+  return render_template('application_sbumitted.html', application=data, job=job)
 
 
 @app.route('/faqs')
--- database.py
+++ database.py
@@ -4,52 +4,60 @@ import os
 timeout = 10
 
 
+def get_db_connection():
+  return pymysql.connect(
+      charset="utf8mb4",
+      connect_timeout=timeout,
+      cursorclass=pymysql.cursors.DictCursor,
+      db=os.environ['DB_CONN_NAME'],
+      host=os.environ['DB_CONN_HOSTNAME'],
+      password=os.environ['DB_CONN_PASS'],
+      read_timeout=timeout,
+      port=11166,
+      user=os.environ['DB_CONN_USER'],
+      write_timeout=timeout,
+  )
+
+
 def load_jobs_from_db():
+  connection = None
   try:
-    connection = pymysql.connect(
-        charset="utf8mb4",
-        connect_timeout=timeout,
-        cursorclass=pymysql.cursors.DictCursor,
-        db=os.environ['DB_CONN_NAME'],
-        host=os.environ['DB_CONN_HOSTNAME'],
-        password=os.environ['DB_CONN_PASS'],
-        read_timeout=timeout,
-        port=11166,
-        user=os.environ['DB_CONN_USER'],
-        write_timeout=timeout,
-    )
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM jobs")
-    result = cursor.fetchall()
-    connection.close()
+    connection = get_db_connection()
+    with connection.cursor() as cursor:
+      cursor.execute("SELECT * FROM jobs")
+      result = cursor.fetchall()
+    return result
   finally:
-    pass
-    
-  return result
+    if connection:
+      connection.close()
 
 
-def load_job_with_id(id):
+def load_job_with_id(job_id):
+  connection = None
   try:
-    connection = pymysql.connect(
-        charset="utf8mb4",
-        connect_timeout=timeout,
-        cursorclass=pymysql.cursors.DictCursor,
-        db=os.environ['DB_CONN_NAME'],
-        host=os.environ['DB_CONN_HOSTNAME'],
-        password=os.environ['DB_CONN_PASS'],
-        read_timeout=timeout,
-        port=11166,
-        user=os.environ['DB_CONN_USER'],
-        write_timeout=timeout,
-    )
-    cursor = connection.cursor()
-    cursor.execute(f"SELECT * FROM jobs WHERE id = {id}")
-    result = cursor.fetchall()
-    connection.close()
+    connection = get_db_connection()
+    with connection.cursor() as cursor:
+      cursor.execute("SELECT * FROM jobs WHERE id = %s", (job_id, ))
+      result = cursor.fetchall()
+    return result[0] if result else None
   finally:
-    pass
-    
-  if len(result) == 0:
-    return None
-  else:
-    return result[0]
+    if connection:
+      connection.close()
+
+
+def add_application_to_db(job_id, data):
+  connection = None
+  try:
+    connection = get_db_connection()
+    with connection.cursor() as cursor:
+      cursor.execute(
+          """
+                INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url)
+                VALUES (%s, %s, %s, %s, %s, %s, %s)
+                """,
+          (job_id, data['full_name'], data['email'], data['linkedin_url'],
+           data['education'], data['work_experience'], data['resume_url']))
+    connection.commit()
+  finally:
+    if connection:
+      connection.close()
